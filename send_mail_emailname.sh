0fc0b43da05baebfbf122ce8fcf88a76a703068b
AceCookies <100018951+AceCookies@users.noreply.github.com>
2025-06-22 22:31:23 +0200

    Create send_mail_emailname.sh

---
+++ send_mail_emailname.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+EMAIL="Adresse email"
+SUBJECT="$1"
+BODY="$2"
+MSMTP_CONF="/etc/nut/msmtp/msmtprc-nut"
+
+echo -e "Subject: $SUBJECT\n\n$BODY" | msmtp -C "$MSMTP_CONF" -a sendgrid "$EMAIL"
